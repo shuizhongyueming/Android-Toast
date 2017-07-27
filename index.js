6b2118b1308d2eee293e567d14fd767d92bedb61
shuizhongyueming <shuizhongyueming@gmail.com>
2017-07-27 16:25:28 +0800

    调整toast的参数，方便调用

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ import './src/android_toast.css';
 import AndroidToast from './src/android_toast.js';
 
 export default function(option){
+    if (typeof option === 'string') {
+        option = {
+            content: option
+        };
+    }
+
     return new AndroidToast(option);
 };
